135d3d4e55ce213a7fe700ddcb0c2f2a82da4a55
Airy <Airy@emt.com>
2021-06-06 15:51:37 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author Wu You
+ * @Date 2021/6/6 15:45
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}

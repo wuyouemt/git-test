2fe95b68a4562057c4940919865f1b292fadbe46
Airy <Airy@emt.com>
2021-06-06 16:03:12 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello github");
         System.out.println("hello gitee");
+        System.out.println("hello emt");
     }
 }
